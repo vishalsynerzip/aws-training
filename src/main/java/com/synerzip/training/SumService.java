0b03daa9f5b19d15344ef2315a4903b62b017414
Ashutosh Kumar <ashutosh@synerzip.com>
2014-01-10 09:06:40 +0530

    added issues in the code for reports

--- src/main/java/com/synerzip/training/SumService.java
+++ src/main/java/com/synerzip/training/SumService.java
@@ -12,11 +12,15 @@ public class SumService
     
     public static double sum(double a, double b) 
     {
+        Long lb = null;
+        System.out.println(lb.doubleValue());
         return a + b;
     }
     
     public static double subtract(double a, double b) 
     {
+        int x = 100;
+       
         return a - b;
     }
 
